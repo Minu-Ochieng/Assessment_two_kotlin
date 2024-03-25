047b8a1972aee01defd6ee3c642c758451f6a86d
Minu-Ochieng <minuochieng@gmail.com>
2024-03-25 07:48:56 +0300

    kotlin assessement 2

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,41 @@
+fun main() {
+
+arrNums(arrayOf(21,17,42,45,12))
+    volume(5.9)
+}
+//Write and invoke a function that given the string “Barnie bakes brown bagels
+//and buns” prints out a list of all the other words and characters making up the
+//string excluding all possible occurrences of the letter b.
+
+fun printOut(word:String){
+    println("Barnie bakes brown bagels and buns ")
+}
+
+//Write and invoke one function that takes in an array of integers and returns
+//these 3 values: sum, count and average of all the elements.
+
+fun arrNums(array: Array<Int>){
+    var sum = array.sum()
+    println(sum)
+    var countElement = array.count()
+    println(countElement)
+     var ave = array.average()
+    println(ave.toDouble())
+}
+//The volume of a sphere is calculated using the formula below
+//V = 4/3 π r
+//3
+//Write and invoke a function that is capable of accurately calculating the
+//volume of any sphere given its radius. Use 3.14159 as π
+
+fun volume(radius:Double){
+    var vol = 4 /3 * 3.14159
+    var area = vol * radius *radius * radius
+    println(area)
+}
+//A palindrome is a string that reads the same forwards and backwards e.g
+//madam, wow, kayak. Write and invoke a function:
+//isPalindrome(word: String): Boolean
+//that takes in a single word and returns true or false depending on whether the
+//word is a palindrome.
+
